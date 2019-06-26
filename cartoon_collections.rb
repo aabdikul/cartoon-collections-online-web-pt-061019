626d72f65bc902feb3d5447a2d0a827cf737d7cf
Aida Abdikulova <abdikulova@gmail.com>
2019-06-26 03:06:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,15 @@ def summon_captain_planet(array)# code an argument here
   end# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+ array.each do |word|
+   if word.length > 4
+     puts true
+   else
+     puts false
+   end
+ end
+ end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
