f515aad515daa9ad86f82f20399f6c1063bfb4c4
Aida Abdikulova <abdikulova@gmail.com>
2019-06-26 02:48:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(array)
 end
 end
 
-def summon_captain_planet()# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  array.collect do |element|
+    element.capitalize << "!"# Your code here
 end
 
 def long_planeteer_calls# code an argument here
