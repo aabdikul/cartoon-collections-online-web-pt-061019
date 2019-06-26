b9971ab9c830d0455df15dbb1269586fa540bcbf
Aida Abdikulova <abdikulova@gmail.com>
2019-06-26 03:22:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,9 @@ def long_planeteer_calls(array)
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   new_array = []
+  i = 0
+  while i < array.length
     new_array << yield(array.include?(cheese_types))
   end
   new_array
-end
 end
