e3ca735f1f087df290974af83c1569030b9711fa
Aida Abdikulova <abdikulova@gmail.com>
2019-06-26 03:38:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def long_planeteer_calls(array)
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect do |item|
-    item.include?[cheese_types]
+    cheese_types.inclue?(item)
   end
 end
